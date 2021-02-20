133c2b76b5d8f428b23c56f7edce2124bbe8e37c
Shayan Sadar <shayansadar37@gmail.com>
2021-02-19 20:49:47 -0600

    Add expand/collapse

--- components/AboutPage/SideIntro.vue
+++ components/AboutPage/SideIntro.vue
@@ -5,11 +5,16 @@
 			<v-card-title style="word-break: break-word">Introduction</v-card-title>
 			<v-card-text
 				style="font-family: 'Montserrat', sans-serif"
-				v-html="whoAmI"
+				v-html="finalContent"
 			></v-card-text>
 			<v-card-actions>
 				<v-spacer></v-spacer>
-				<v-btn color="primary"> Know More --></v-btn>
+				<v-icon v-if="icon" color="primary" @click="modifyContent(icon)"
+					>mdi-chevron-down</v-icon
+				>
+				<v-icon v-else color="primary" @click="modifyContent(icon)"
+					>mdi-chevron-up</v-icon
+				>
 				<v-spacer></v-spacer>
 			</v-card-actions>
 		</v-card>
@@ -20,9 +25,30 @@
 export default {
 	data() {
 		return {
+			finalContent: '',
 			whoAmI:
-				"<p>Hi, I'm Shayan. Trying to make the society a better place through technology, currently based in 📍Dallas,TX. Alumna 🎓 of <em>The University of Texas at Dallas</em> and <em>University of Pune</em>. Intrigued by sports, traveling, great food, strategy, finance, history and even better conversations. Seeking to be inspired, to envision the unlikely, to work hard for things that are worth it, and to be surrounded by those who bring out the best in me.</p>"
+				"<p>Hi, I'm Shayan. Trying to make the society a better place through technology, currently based in 📍Dallas,TX. Alumna 🎓 of <em>The University of Texas at Dallas</em> and <em>University of Pune</em>. Intrigued by sports, traveling, great food, strategy, finance, history and even better conversations. Seeking to be inspired, to envision the unlikely, to work hard for things that are worth it, and to be surrounded by those who bring out the best in me.</p>",
+			icon: 1 //down
+		}
+	},
+	mounted() {
+		this.finalContent = this.whoAmI
+	},
+	methods: {
+		modifyContent(show) {
+			if (show)
+				this.finalContent =
+					this.whoAmI +
+					"<p>Thank you for your curosity to know more, but I don't have anything else at the moment.</p>"
+			else this.finalContent = this.whoAmI
+			this.icon = !this.icon
 		}
 	}
 }
 </script>
+
+<style lang="scss" scoped>
+.v-icon {
+	text-decoration: none;
+}
+</style>
