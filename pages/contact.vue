e7c81d110cec92538e87f9e5355f22c699ff6c4e
Shayan Sadar <shayansadar37@gmail.com>
2021-05-15 10:53:53 -0500

    Setup contact page

--- pages/contact.vue
+++ pages/contact.vue
@@ -1,37 +1,40 @@
 <template>
-	<v-layout class="layout" align-center justify-center>
-		<v-col cols="6">
-			<h1 class="contact-title text-center pa-4">GET IN TOUCH</h1>
-			<v-form ref="form" v-model="valid" lazy-validation>
-				<v-text-field
-					v-model="name"
-					:counter="20"
-					:rules="nameRules"
-					label="Name"
-					required
-				></v-text-field>
-
-				<v-text-field
-					v-model="email"
-					:rules="emailRules"
-					label="E-mail"
-					required
-				></v-text-field>
-
-				<v-textarea v-model="bio" color="teal">
-					<template #label>
-						<div>Message <small>(optional)</small></div>
-					</template>
-				</v-textarea>
-
-				<v-btn :disabled="!valid" color="success" class="mr-4" @click="validate">
-					Submit
+	<v-container fluid class="contact-container mb-10">
+		<v-form ref="form" v-model="valid" lazy-validation>
+			<v-row class="contact-title text-center mb-3">
+				<v-btn class="back-button ml-2 pa-2" to="/">
+					<v-icon color="black"> mdi-arrow-left </v-icon>
 				</v-btn>
+				<h1 class="contact-header pa-4">Get In Touch</h1>
+			</v-row>
+			<v-text-field
+				v-model="name"
+				:counter="20"
+				:rules="nameRules"
+				label="Name"
+				required
+			></v-text-field>
 
-				<v-btn color="error" class="mr-4" @click="reset"> Reset </v-btn>
-			</v-form>
-		</v-col>
-	</v-layout>
+			<v-text-field
+				v-model="email"
+				:rules="emailRules"
+				label="E-mail"
+				required
+			></v-text-field>
+
+			<v-textarea v-model="bio" color="teal">
+				<template #label>
+					<div>Message <small>(optional)</small></div>
+				</template>
+			</v-textarea>
+
+			<v-btn :disabled="!valid" color="success" class="mr-4" @click="validate">
+				Submit
+			</v-btn>
+
+			<v-btn color="error" class="mr-4" @click="reset"> Reset </v-btn>
+		</v-form>
+	</v-container>
 </template>
 
 <script>
@@ -63,18 +66,28 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-.layout {
-	padding-top: 8%;
+.contact-container {
+	background-color: white;
+	height: 100%;
+	width: 50%;
 }
 .v-form {
-	border: 1px solid black;
 	padding: 10px;
 }
 .contact-title {
-	color: purple;
-	font-size: 48px;
-	font-style: italic;
-	font-family: 'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande',
-		'Lucida Sans Unicode', Geneva, Verdana, sans-serif;
+	font-family: 'Farro', sans-serif;
+	font-weight: 500;
+	letter-spacing: 0.0125em;
+	line-height: 2rem;
+	font-size: 24px;
+	border: 1px;
+	background-color: #596678;
+	color: white;
+}
+.contact-header {
+	margin: auto;
+}
+.back-button {
+	margin-block: auto;
 }
 </style>
