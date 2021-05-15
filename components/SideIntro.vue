159844320ecc6854f4b3eb17352c4a5b3ffc2166
Shayan Sadar <shayansadar37@gmail.com>
2021-05-15 14:09:46 -0500

    Typo

--- components/SideIntro.vue
+++ components/SideIntro.vue
@@ -34,7 +34,7 @@ export default {
 		return {
 			finalContent: '',
 			whoAmI:
-				"<p>Hi, I'm Shayan. Trying to make the society a better place through technology, currently based in 📍Dallas,TX. Alumna 🎓 of <em>The University of Texas at Dallas</em> and <em>University of Pune</em>. Intrigued by sports, travelling, great food, strategy, finance, history and even better conversations. Seeking to be inspired, to envision the unlikely, to work hard for things that are worth it, and to be surrounded by those who bring out the best in me.</p>",
+				"<p>Hi, I'm Shayan. Trying to make the society a better place through technology, currently based in 📍Dallas,TX. Alumnus 🎓 of <em>The University of Texas at Dallas</em> and <em>University of Pune</em>. Intrigued by sports, travelling, great food, strategy, finance, history and even better conversations. Seeking to be inspired, to envision the unlikely, to work hard for things that are worth it, and to be surrounded by those who bring out the best in me.</p>",
 			icon: 1, //down,
 			isSmallScreen: false
 		}
