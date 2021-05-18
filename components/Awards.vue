40c72ecd5745ea9336e81b12abcbbd3fa9f6dbef
Shayan Sadar <shayansadar37@gmail.com>
2021-05-17 21:48:26 -0500

    Use primitive value as key

--- components/Awards.vue
+++ components/Awards.vue
@@ -9,7 +9,7 @@
 			AWARDS
 		</p>
 		<ul>
-			<li v-for="award in awards" :key="award" class="award-list row">
+			<li v-for="award in awards" :key="award.id" class="award-list row">
 				{{ award.title }} &#183;
 				<p class="years">&nbsp; {{ award.year }}</p>
 			</li>
@@ -23,26 +23,32 @@ export default {
 		return {
 			awards: [
 				{
+					id: 1,
 					title: 'HackDFW Challenge 2nd Runners Up',
 					year: 2019
 				},
 				{
+					id: 2,
 					title: 'RACE 2018 Academic Conference - Best Research Paper',
 					year: 2018
 				},
 				{
+					id: 3,
 					title: 'Runner-Up Trophy Basketball',
 					year: 2016
 				},
 				{
+					id: 4,
 					title: 'Best Sportsman in High School',
 					year: 2012
 				},
 				{
+					id: 5,
 					title: 'Most Promising Student By Ishanya Foundation',
 					year: 2011
 				},
 				{
+					id: 6,
 					title: 'Best Basketball Player',
 					year: 2010
 				}
